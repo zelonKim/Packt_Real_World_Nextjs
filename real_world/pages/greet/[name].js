94d8ca334c994aa8ea544cd5848c3a23ba04e4e9
zelonKim <ksz18601@gmail.com>
2023-07-28 21:11:37 +0900

    CSR & SSG & ISR

---
+++ real_world/pages/greet/[name].js
@@ -0,0 +1,29 @@
+/* export async function getServerSideProps({ params }) {
+    const { name } = params;
+    return {
+        props: {
+            name
+        }
+    }
+}
+
+function Greet(props) {
+    return (
+        <h1> Hello, {props.name} </h1>
+    )
+}
+export default Greet; */
+
+///////////////////
+
+
+import { useRouter } from 'next/router'
+
+function Greet() {
+    const { query } = useRouter();
+    
+    console.log(query) 
+    return <h1>Hello {query.name} </h1> 
+}
+export default Greet;
+
--- real_world/pages/index.js
+++ real_world/pages/index.js
@@ -1,7 +1,8 @@
-
 // 'getServerSideProps' function makes the page more dynamic such as calling external API or database 
 
-export async function getServerSideProps() {
+// import { useEffect } from "react";
+
+/* export async function getServerSideProps() {
   const userRequest = await fetch('https://example.com/api/user')
   const userData = awiat userRequest.json()
 
@@ -17,4 +18,83 @@ function IndexPage(props) {
     <div>hello, {props.user.name}</div>
   )
 }
-export default IndexPage 
+export default IndexPage  */
+
+
+////////////////////////////
+
+/* 
+export async function getStaticProps() {
+  const userRequest = await fetch('https://example.com/api/user')
+  const userData = awiat userRequest.json()
+
+  const dashboardRequest = await fetch('/api/dashboard')
+  const dashboardData = await dashboardRequest.json()
+
+  return {
+    props: {
+      user: userData,
+      data: dashboardData,
+    },
+    revalidate: 600 // cache it for up to 600 seconds using SSG and ISR
+  }
+}
+
+function IndexPage(props) {
+  return (
+    <div>
+      <Dashboard
+        user = {props.user}
+        data = {props.data}
+      />
+      </div>
+      )
+}
+export default IndexPage  */
+
+////////////////
+
+
+/* function EffectPage() {
+  const [isClient, setIsClient] = useState(false)
+  
+  useEffect(()=> {
+    setIsClient(true)
+  }, [])
+
+  return (
+    <div>
+      {isClient && (<Highlight code={"console.log('Hello, world!')"} language='js' />)}
+    </div>
+  )
+}
+export default EffectPage; */
+
+
+////////////
+
+
+/* function IndexPage() {
+  const side = process.window ? 'client' : 'server';
+  return <div> You`re currently on the {side}-side </div>
+}
+export default IndexPage; */
+
+
+///////////
+
+/* 
+import dynamic from 'next/dynamic'
+const Highlight = dynamic( () => import ('../components/Highlight'), { ssr: false })
+
+import styles from '../styles/Home.module.css'
+
+function DynamicPage() {
+  return (
+    <div className={styles.main}>
+      <Highlight code={"console.log('Hello, world!')"} language='js' />
+    </div>
+  )
+} */
+
+
